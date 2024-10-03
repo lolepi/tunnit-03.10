f709d64766eda4f5b85dd5fadc393ea2f09826af
lolepi <loviisa.lepik@perg.ee>
2024-10-03 10:57:56 +0300

    Add files via upload

--- tunnitoo.py
+++ tunnitoo.py
@@ -2,14 +2,14 @@
 
 # ül.1
 # konsooli printimine prindi konsooli tervitus
-
+# print("Tere!")
 
 # ül.2
 # muuda oma kood nii et see küsiks kasutajalt nime ja tervitaks teda
 # vihje: sisend saab kutsuda funktsiooniga input()
 # kui on vaja esitada konkreetne küsimus
 # siis selle saab sõne andmetüübina kirjutada funtsiooni sisse
-
+# print(f"Tere, {input("Mis on sinu nimi?")}!")
 # ül.2.1
 # muuda oma kood nii et kasutaja ei peaks mõtlema liigsetele tühikutele ja suurele algustähele
 
@@ -17,14 +17,14 @@
 # ül.3.1
 # kontrolli oma teadmisi. Mida väljastavad kosooli kolm allolevat koodi
 # vastus kirjuta noole taha, seejärel kustuta rea alguses olev# ja kontorlli enast
-# print(type(7))  # ->
-# print(type("siin on mingisugune lause"))  # ->
-# print(type(7.0))  # ->
+# print(type(7))  # ->int
+# print(type("siin on mingisugune lause"))  # ->string
+# print(type(7.0))  # ->float
 
 
 # ül.3.2
 # kirjuta sulgudesse vastav pythoni termin
-# sõne (), täisarv(), ratsionaalarv()
+# sõne (str), täisarv(int), ratsionaalarv(float)
 
 # MUUTUJAD - kuidas hoida andmeid mälus
 # matemaatikast teame muutjuaid x ja y, kuid samuti teame et tegelikult võivad nad
@@ -34,6 +34,8 @@
 # selle jaoks määrame muutjuale nime ning seejärel anname talle = märgi abil väärtuse
 # ül.4
 # mõtle välja loogiline nimetus muutujale, mis määrab sinu vanust, pane see kirja
+minu_vanus = 16
 # Python 3.0 oli väljastatud aastal 2008
-release_year = 2008
+release_year_of_phyton = 2008
 # mõtle välja loogiline nimetus muutujale, mis määrab pyhtoni vanust matemaatilise tehtena, pane see kirja
+phytoni_vanus = 2024 - release_year_of_phyton
